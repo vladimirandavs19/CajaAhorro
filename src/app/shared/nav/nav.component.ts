bce7d1b26085ce1dd3aebb69935e89f5ffe7ac42
vladimirandavs19 <vladi_vinci@hotmail.com>
2021-05-20 12:59:51 -0500

    NUevos componentes

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuModel } from '../../models/menu';
 
 @Component({
   selector: 'app-nav',
@@ -7,12 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   ver = true;
-  constructor() { }
+  menu: MenuModel[];
+  constructor() {
+    this.menu = [];
+  }
 
   ngOnInit(): void {
     let boton = document.getElementById('btnCollapseMenu');
     console.log(boton);
     boton?.click();
+
+    this.menu = [
+      {
+        url: '',
+        icon: 'lni lni-home',
+        name: 'Dashboard',
+      },
+      {
+        url: '',
+        icon: 'lni lni-user',
+        name: 'Clientes',
+      },
+      {
+        url: '',
+        icon: 'lni lni-consulting',
+        name: 'Préstamos',
+      },
+      {
+        url: '',
+        icon: 'lni lni-invest-monitor',
+        name: 'Estado de Cuenta'
+      },
+      // Para los casos que tenga submenu
+      // {
+      //   url: 'javascript:void(0)',
+      //   icon: 'ion-monitor',
+      //   name: 'Ui elements',
+      //   children: [
+      //     {
+      //       url: '',
+      //       icon: 'ion-monitor',
+      //       name: 'Buttons',
+      //     }
+      //   ],
+      // }
+    ]
   }
 
 }
